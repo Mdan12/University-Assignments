31410b08d4a862d5c2e9deec4c526bd2b1b09713
mdan_12 <magnusde93@gmail.com>
2022-09-08 18:14:42 +0000

    Assignment 2 finished

--- Web-Development-1/Ex02/gogn/baekur.py
+++ Web-Development-1/Ex02/gogn/baekur.py
@@ -1,7 +1,3 @@
-from itertools import chain
-
 with open("baekur.csv") as f:
     for line in f:
-        asd =  line.split(";")
-        string = [s for S in asd for s in S]
-        print(string)
+        print(f"<li>{line.strip()}</li>")
