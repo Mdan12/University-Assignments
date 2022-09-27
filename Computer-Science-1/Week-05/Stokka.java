ceb1749346ed4debc62e17a64f33fdf844ee69ce
mdan_12 <magnusde93@gmail.com>
2022-09-27 21:51:40 +0000

    WebDev assignment 4

--- Computer-Science-1/Week-05/Stokka.java
+++ Computer-Science-1/Week-05/Stokka.java
@@ -2,11 +2,28 @@
  *  Nafn    : Magnús Daníel Budai Einarsson
  *  T-póstur: mde2@hi.is
  *
- *  Lýsing  :
- *
+ *  Lýsing  : Tekur inn tölur sem args, setur í array og gerir svo random röðun
+ *  á því array og prentar því á console.
  *
  *****************************************************************************/
 public class Stokka {
 
   public static void main(String[] args) {
+    int N = args.length;
+    int[] fylki = new int[N];
+
+    for (int i = 0; i < N; i++){
+      fylki[i] = Integer.parseInt(args[i]);
+    }
+
+    for (int i = 0; i < N; i++){
+      int r = StdRandom.uniform(i, N);
+      int t = fylki[i];
+      fylki[i] = fylki[r];
+      fylki[r] = t;
+      System.out.print(fylki[i]+ " ");
+    }
+  }
+}
+
 
