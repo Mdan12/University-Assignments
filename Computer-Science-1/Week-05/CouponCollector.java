f7e5bced6e52ee8946b265fcb3ab4ebcf291df19
mdan_12 <magnusde93@gmail.com>
2022-10-06 19:48:48 +0000

    computer science assignment

--- Computer-Science-1/Week-05/CouponCollector.java
+++ Computer-Science-1/Week-05/CouponCollector.java
@@ -1,8 +1,7 @@
 public class CouponCollector {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);       // number of card types
-        boolean[] isCollected
-                = new boolean[n];  // isCollected[i] = true if card i has been collected
+        boolean[] isCollected= new boolean[n];  // isCollected[i] = true if card i has been collected
         int count = 0;                           // total number of cards collected
         int distinct = 0;                        // number of distinct cards
         // repeatedly choose a random card and check whether it's a new one
