129ca1aa3397947f323c9d798b364d50437ea52d
mdan_12 <magnusde93@gmail.com>
2022-10-12 06:18:46 +0000

    minor fixes to Triangle in CS

--- Computer-Science-1/Week-04/Triangle.java
+++ Computer-Science-1/Week-04/Triangle.java
@@ -10,9 +10,10 @@
  *****************************************************************************/
 public class Triangle {
     public static void main(String[] args) {
-        int n = Integer.parseInt(args[0]);
-        for(int i=1;i<=n;i++){  
-            for(int j=1;j<=i;j++){  
+        int n = 10;
+        int k = 3;
+        for(int i=0;i<=n;i+=2){  
+            for(int j=0;j<k;j++){  
                 System.out.print("*");  
             }
             System.out.println();
